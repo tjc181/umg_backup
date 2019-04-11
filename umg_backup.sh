8826dbefc592e3746cf891787ea6385d7b4d2d4f
Tom Canich <tjc181@psu.edu>
2019-04-11 14:49:34 -0400

    Updated to accept an arbitrary number of group name arguments.  Each will be processed and result written to standard out.  Failures are accumulated and reported via the exit code.

--- umg_backup.sh
+++ umg_backup.sh
@@ -4,28 +4,31 @@ ldaphost="dirapps.aset.psu.edu"
 ldapbase="dc=psu,dc=edu"
 ldapargs="-LLL -x -h $ldaphost -b $ldapbase"
 ldapattr="psMemberOf"
-umg=$1
-filter="$ldapattr=cn=$umg,$ldapbase uid"
+args=$@
 today=$(date +%Y%m%d)
-output="/home/geosc/tjc181/backup/umg/$(echo $umg | sed -e 's/\//_/g')-$today"
+fail=0
 
-if [ "$umg" == "" ]
+if [ "${#args}" -eq "0" ]
 then
-  echo "Usage: umg_backup.sh group"
+  echo "Usage: umg_backup.sh group [group] ..."
   exit 99
 fi
 
-ldapsearch $ldapargs $filter | \
-  sed -e '/^dn.*/d' -e '/^$/d' -e 's/^uid: \(.*\)/\1/' > $output
-result=$?
+for umg in "$@"
+do
+  filter="$ldapattr=cn=$umg,$ldapbase uid"
+  output="/home/geosc/tjc181/backup/umg/$(echo $umg | sed -e 's/\//_/g')-$today"
+  ldapsearch $ldapargs $filter | \
+    sed -e '/^dn.*/d' -e '/^$/d' -e 's/^uid: \(.*\)/\1/' > $output
+  result=$?
+  echo -n "Backup of $umg: "
+  if [ "$result" -eq "0" ]
+  then
+    echo "success"
+  else
+    echo "failure"
+    fail=$((fail + 1))
+  fi
+done
 
-echo -n "Backup of $umg: "
-if [ "$result" -eq "0" ]
-then
-  echo "success"
-else
-  echo "failure"
-fi
-
-exit $result
-
+exit $fail
